730bad25a9baf2d6f5f8c8bc4810ce6daf15be63
Szihalmi Levente <b3szilev@vasvari.org>
2025-09-29 14:04:29 +0200

    masodik feladat kesz

---
+++ masodik.py
@@ -0,0 +1,4 @@
+a=float(input("Kérem a téglalap egyik oldalának hosszát!"))
+b=float(input("Kérem a téglalap másik oldalának hosszát!"))
+print("A téglalap kerülete: ",2*(a+b))
+print("A téglalap területe: ",round(a*b,2))
